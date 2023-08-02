4702786d17d6409c5a9a40292f0aaf8b2360fcd7
NicolasAyal <javier17utn@gmail.com>
2023-08-02 19:39:19 -0300

    env2

--- connectDB.js
+++ connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require ("mongoose");
 const connectDB = async ()=>{
     try {
         mongoose.set('strictQuery',false);
-        const conn = await mongoose.connect('mongodb+srv://javier17utn:N7dEYrAXrD8vBykY@cluster0.8ig9wxj.mongodb.net/diary');
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
         console.log(`Database Connected ${conn.connection.host}`)
     } catch (error) {
         console.log(error)
