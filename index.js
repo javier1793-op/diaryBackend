c3a095bb762a71b735bceb08ae03c1fd5fa5df20
NicolasAyal <javier17utn@gmail.com>
2023-08-09 09:50:38 -0300

    prueba-cyclis

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-require("dotenv").config();
-const cors = require("cors");
-const express = require("express");
-const connectDB = require("./connectDB");
 
-const apiRouter = require("./Routes/api");
+const express = require("express");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-connectDB();
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 
 app.use("/api", apiRouter)
 
